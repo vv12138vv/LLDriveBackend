282c64b6d969ef6ef3d6e363b4077863f7bd6093
vv12138vv <jgss987611676@outlook.com>
2023-09-22 12:22:13 +0800

    better token service

--- src/main/java/com/lldrive/domain/consts/Const.java
+++ src/main/java/com/lldrive/domain/consts/Const.java
@@ -7,6 +7,6 @@ public class Const {
     public static final int UUID_LENGTH=32;
 
     public static final int FILE_NAME_LENGTH=32;
-    public static final int LOGIN_VALID_TIME=15;
+    public static final int LOGIN_VALID_TIME=20;//20min
     public static final long CHUNK_SIZE=5242880;//5MB
 }
