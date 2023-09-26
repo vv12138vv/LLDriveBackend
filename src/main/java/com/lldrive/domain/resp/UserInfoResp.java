bfae8b162fa6e6d6c758cb90d6a4d925345cb901
vv12138vv <jgss987611676@outlook.com>
2023-09-26 10:53:57 +0800

    add capacity to userInfoResp

--- src/main/java/com/lldrive/domain/resp/UserInfoResp.java
+++ src/main/java/com/lldrive/domain/resp/UserInfoResp.java
@@ -1,6 +1,7 @@
 package com.lldrive.domain.resp;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lldrive.domain.entity.Repo;
 import com.lldrive.domain.entity.User;
 import lombok.Data;
 
@@ -12,11 +13,17 @@ public class UserInfoResp {
     private Boolean isBanned;
     @JsonProperty("is_admin")
     private Boolean isAdmin;
+    @JsonProperty("cur_capacity")
+    private Long curCapacity;
+    @JsonProperty("max_capacity")
+    private Long maxCapacity;
 
-    public UserInfoResp(User user){
+    public UserInfoResp(User user, Repo repo){
         this.username=user.getUsername();
         this.email=user.getEmail();
         this.isBanned =user.getIsBanned();
         this.isAdmin =user.getIsAdmin();
+        this.curCapacity= repo.getCurCapacity();
+        this.maxCapacity=repo.getMaxCapacity();
     }
 }
