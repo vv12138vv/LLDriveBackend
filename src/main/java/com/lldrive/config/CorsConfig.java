e1a456ef3bfbaec9bfdf75d02f9912d9fc59259f
vv12138vv <jgss987611676@outlook.com>
2023-09-26 18:45:51 +0800

    edit cors config

--- src/main/java/com/lldrive/config/CorsConfig.java
+++ src/main/java/com/lldrive/config/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig{
     @Bean
     public CorsFilter corsFilter(){
         CorsConfiguration config=new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://localhost:5174");
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
