64708539f5089bb2c1424cb69987f08d98f7c930
vv12138vv <jgss987611676@outlook.com>
2023-10-01 06:14:47 +0800

    pack up

--- src/test/java/com/lldrive/UserFileMapperTest.java
+++ src/test/java/com/lldrive/UserFileMapperTest.java
@@ -14,29 +14,29 @@ import java.util.List;
 
 @SpringBootTest
 public class UserFileMapperTest {
-    @Autowired
-    UserFileMapper userFileMapper;
-
-    @Autowired
-    UserFileService userFileService;
-
-    @Autowired
-    UserService userService;
-    @Test
-    public void testSearch(){
-        String repoId="80b3a2230fa6474cb49a88971eedb0f9";
-        List<UserFile> searchResult=userFileMapper.selectUserFilesByRepoIdAndFilename(repoId,"Folder");
-        for(UserFile userFile:searchResult){
-            System.out.println(userFile);
-        }
-    }
-
-    @Test
-    public void testDeleteAndRecycle(){
-        String repoId="80b3a2230fa6474cb49a88971eedb0f9";
-        CommonResp  userResp =userService.findUser("test");
-        User user=(User) userResp.getData();
-        userFileService.deleteUserFile(user,"a3722b37b64a42b9bb290bf5ad94c2f0");
-        List<UserFile> deletedFiles=userFileMapper.selectDeletedFiles(repoId);
-    }
+//    @Autowired
+//    UserFileMapper userFileMapper;
+//
+//    @Autowired
+//    UserFileService userFileService;
+//
+//    @Autowired
+//    UserService userService;
+//    @Test
+//    public void testSearch(){
+//        String repoId="80b3a2230fa6474cb49a88971eedb0f9";
+//        List<UserFile> searchResult=userFileMapper.selectUserFilesByRepoIdAndFilename(repoId,"Folder");
+//        for(UserFile userFile:searchResult){
+//            System.out.println(userFile);
+//        }
+//    }
+//
+//    @Test
+//    public void testDeleteAndRecycle(){
+//        String repoId="80b3a2230fa6474cb49a88971eedb0f9";
+//        CommonResp  userResp =userService.findUser("test");
+//        User user=(User) userResp.getData();
+//        userFileService.deleteUserFile(user,"a3722b37b64a42b9bb290bf5ad94c2f0");
+//        List<UserFile> deletedFiles=userFileMapper.selectDeletedFiles(repoId);
+//    }
 }
